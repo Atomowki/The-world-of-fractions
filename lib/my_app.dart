5c296477afb4479ecbae30a2e91032446f0c7bfe
PCissewski <cissek99@gmail.com>
2023-04-12 18:29:25 +0200

    initial commit

---
+++ lib/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Świat ułamków',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Świat ułamków'),
+    );
+  }
+}
