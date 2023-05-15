f87270a7d47f95cbf0486dde13c6971f77737b44
Paweł Cissewski <48312059+PCissewski@users.noreply.github.com>
2023-05-15 11:14:04 +0200

    added menu and routing to pages (#1)

---
+++ lib/common_denominator_page/common_denominator_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:the_world_of_fractions/reusables/my_app_bar.dart';
+
+class CommonDenominatorPage extends StatelessWidget {
+  const CommonDenominatorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: PrimaryAppBar(),
+      body: Placeholder(),
+    );
+  }
+}
---
+++ lib/reducing_fractions_page/reducing_fractions_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:the_world_of_fractions/reusables/my_app_bar.dart';
+
+class ReducingFractionsPage extends StatelessWidget {
+  const ReducingFractionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: PrimaryAppBar(),
+      body: Placeholder(),
+    );
+  }
+}
