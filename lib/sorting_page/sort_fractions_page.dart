e4958b1a988c7b4d27d4b41a3573414603d6ceb9
PCissewski <cissek99@gmail.com>
2023-06-11 21:01:04 +0200

    added sorting

--- lib/sorting_page/sort_fractions_page.dart
+++ lib/sorting_page/sort_fractions_page.dart
@@ -14,45 +14,58 @@ class SortFractionsPage extends StatefulWidget {
 
 class _SortFractionsPageState extends State<SortFractionsPage> {
   List<String> symbols = ["<", ">"];
+  Fraction f1 = const Fraction(upperSymbol: "1", downSymbol: "2");
+  Fraction f2 = const Fraction(upperSymbol: "2", downSymbol: "3");
 
   @override
   Widget build(BuildContext context) {
+    String symbol = symbols[0];
+
+    final answer = compareFractions(f1, f2, symbol);
     return Scaffold(
       appBar: const PrimaryAppBar(),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SUPrimaryButton(
-            text: "Sprawadź",
-            onPressed: () {
-              if (true) {
-                Fluttertoast.showToast(
-                  msg: "Udało się",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.CENTER,
-                  timeInSecForIosWeb: 1,
-                  backgroundColor: Colors.blue,
-                  textColor: Colors.white,
-                  fontSize: 16.0,
-                );
-              } else {
-                Fluttertoast.showToast(
-                  msg: "meh",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.CENTER,
-                  timeInSecForIosWeb: 1,
-                  backgroundColor: Colors.red,
-                  textColor: Colors.white,
-                  fontSize: 16.0,
-                );
-              }
-            },
-          )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              f1,
+              Text(
+                symbols[0],
+                style: const TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+              f2,
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SUPrimaryButton(
+                text: "Prawda",
+                onPressed: () {
+                  checkAnswer(f1, f2, answer, symbol, true);
+                },
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              SUPrimaryButton(
+                text: "Nieprawda",
+                onPressed: () {
+                  checkAnswer(f1, f2, answer, symbol, false);
+                },
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
 
-  int compareFractions(Fraction f1, Fraction f2) {
+  bool compareFractions(Fraction f1, Fraction f2, String symbol) {
     var f1Up = int.parse(f1.upperSymbol);
     var f1Dw = int.parse(f1.downSymbol);
 
@@ -64,6 +77,46 @@ class _SortFractionsPageState extends State<SortFractionsPage> {
     print(calc1);
     print(calc2);
     print("wynik = ${calc1.compareTo(calc2)}");
-    return calc1.compareTo(calc2);
+
+    bool answer = false;
+    switch (symbol) {
+      case "<":
+        answer = calc1 < calc2;
+        print("case 1:  ${answer}");
+        break;
+      case ">":
+        answer = calc1 > calc2;
+        print("case 2:  ${answer}");
+        break;
+      default:
+    }
+    print(answer);
+    return answer;
+  }
+
+  void checkAnswer(
+      Fraction f1, Fraction f2, bool answer, String symbol, bool tryAnswer) {
+    bool compare = compareFractions(f1, f2, symbol);
+    if (compare == tryAnswer) {
+      Fluttertoast.showToast(
+        msg: "Udało się",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    } else {
+      Fluttertoast.showToast(
+        msg: "Nie powiodło się",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }
   }
 }
