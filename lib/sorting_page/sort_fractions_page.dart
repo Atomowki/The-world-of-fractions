5a16d235b5d8d0d420731d7635cb642985a98715
PCissewski <cissek99@gmail.com>
2023-06-11 20:15:57 +0200

    added fraction

--- lib/sorting_page/sort_fractions_page.dart
+++ lib/sorting_page/sort_fractions_page.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:the_world_of_fractions/reusables/fraction.dart';
-import 'package:the_world_of_fractions/reusables/my_app_bar.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:the_world_of_fractions/reusables/su_primary_button.dart';
 
-class SortFractionsPage extends StatelessWidget {
+import '../reusables/fraction.dart';
+import '../reusables/my_app_bar.dart';
+
+class SortFractionsPage extends StatefulWidget {
   const SortFractionsPage({super.key});
+
+  @override
+  State<SortFractionsPage> createState() => _SortFractionsPageState();
+}
+
+class _SortFractionsPageState extends State<SortFractionsPage> {
+  List<String> symbols = ["<", ">"];
 
   @override
   Widget build(BuildContext context) {
@@ -11,34 +21,49 @@ class SortFractionsPage extends StatelessWidget {
       appBar: const PrimaryAppBar(),
       body: Column(
         children: [
-          Draggable<Fraction>(
-            feedback: const Fraction(
-              upperSymbol: '2137',
-              downSymbol: '420',
-            ),
-            childWhenDragging: Container(
-              color: Colors.white,
-            ),
-            child: const Fraction(
-              upperSymbol: '2137',
-              downSymbol: '420',
-            ),
-          ),
-          DragTarget<Fraction>(
-            builder: (
-              BuildContext context,
-              List<dynamic> accepted,
-              List<dynamic> rejected,
-            ) {
-              return Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.cyan,
-              );
+          SUPrimaryButton(
+            text: "Sprawadź",
+            onPressed: () {
+              if (true) {
+                Fluttertoast.showToast(
+                  msg: "Udało się",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.blue,
+                  textColor: Colors.white,
+                  fontSize: 16.0,
+                );
+              } else {
+                Fluttertoast.showToast(
+                  msg: "meh",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0,
+                );
+              }
             },
-          ),
+          )
         ],
       ),
     );
+  }
+
+  int compareFractions(Fraction f1, Fraction f2) {
+    var f1Up = int.parse(f1.upperSymbol);
+    var f1Dw = int.parse(f1.downSymbol);
+
+    var f2Up = int.parse(f2.upperSymbol);
+    var f2Dw = int.parse(f2.downSymbol);
+
+    double calc1 = f1Up / f1Dw;
+    double calc2 = f2Up / f2Dw;
+    print(calc1);
+    print(calc2);
+    print("wynik = ${calc1.compareTo(calc2)}");
+    return calc1.compareTo(calc2);
   }
 }
